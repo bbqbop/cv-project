26cd31031b855bf40eae24e75ab75c2998ad6777
bbqbop <dierk.peters@gmail.com>
2023-05-26 11:56:46 -0400

    update data flow for nested objects in arrays

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { PersonalInformation, EducationalExperience, ProfessionalExperience } from './Components/Forms'
+import { Form } from './Components/Forms'
 
 export default class App extends Component {
   constructor(){
     super()
     this.state = {
-      PersonalInformation: {
+      PersonalInformation: [{
         FirstName: {placeholder: 'First Name', data: ''},
         LastName: {placeholder: 'Last Name', data: ''},
         Title: {placeholder:'Title', data: ''}, 
@@ -16,35 +16,54 @@ export default class App extends Component {
         Email: {placeholder:'Email', data: ''},
         PhoneNumber: {placeholder:'Phone Number', data: ''},
         Description: {placeholder:'Description', data: ''},
-      },
-      EducationalExperience: [],
-      ProfessionalExperience: []
+      }],
+      EducationalExperience: [new EducationEntry()],
+      ProfessionalExperience: [new JobEntry()],
     }
   } 
 
-  onChange = (e, subFolder) => {
+  onChange = (e, subFolder, index = 0) => {
     const { value, id } = e.target
-   this.setState(prevState => ({
-      [subFolder]: {
-        ...prevState[subFolder],
-        [id]: {
-          ...prevState[subFolder][id],
-          data: value
-        }
-      }
-    })
+   this.setState(prevState => {
+    const updatedArray = [...prevState[subFolder]];
+    const updatedObject = updatedArray[index];
+
+    updatedObject[id] = {...updatedObject[id], data: value};
+
+    updatedArray[index] = updatedObject;
+   }
    )
-     console.log(this.state);
+   console.log(this.state)
   }
 
   render(){
-
     return (
       <div>
-        <PersonalInformation infos={this.state.PersonalInformation} onChange={this.onChange} />
-        {/* <EducationalExperience />
-        <ProfessionalExperience /> */}
+        <Form data={this.state.PersonalInformation[0]} onChange={this.onChange} section='PersonalInformation' />
+        <Form data={this.state.EducationalExperience[0]} onChange={this.onChange} section='EducationalExperience' />
+        <Form data={this.state.ProfessionalExperience[0]} onChange={this.onChange} section='ProfessionalExperience' />
+
       </div>
     )
+  }
+}
+
+function EducationEntry(){
+  return {
+    SchoolName: {placeholder: 'School Name', data: ''},
+    TitleOfStudy: {placeholder: 'Title of Study', data: ''},
+    Degree: {placeholder: 'Degree', data: ''},
+    From: {placeholder: 'From', data: ''},
+    To: {placeholder: 'To', data: ''},
+  }
+};
+
+function JobEntry(){
+  return {
+    Position: {placeholder: 'Position', data: ''},
+    Company: {placeholder: 'Company', data: ''},
+    City: {placeholder: 'City', data: ''},
+    From: {placeholder: 'From', data: ''},
+    To: {placeholder: 'To', data: ''},
   }
 }
--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -1,63 +1,28 @@
 import React, { Component } from 'react';
 
-export class PersonalInformation extends Component {
+export class Form extends Component {
     constructor(props){
         super(props);
     }
     render(){
-        const { infos, onChange } = this.props;
+        const { data, onChange, section } = this.props;
         function handleChange(e) {
-            onChange(e, 'PersonalInformation')
+            onChange(e, section)
         }
+        const keys = Object.keys(data);
         return (
             <form>
-                <legend>Personal Information</legend>
-                <Input prop = {infos.FirstName} id = {Object.keys(infos)[0]} type = "text" onChange={handleChange}/>
-                <Input prop = {infos.LastName} id = {Object.keys(infos)[1]} type = "text" onChange={handleChange}/>
-                <Input prop = {infos.Title} id = {Object.keys(infos)[2]} type = "text" onChange={handleChange}/>
-                <Input prop = {infos.Address} id = {Object.keys(infos)[3]} type = "text" onChange={handleChange}/>
-                <Input prop = {infos.City} id = {Object.keys(infos)[4]} type = "text" onChange={handleChange}/>
-                <Input prop = {infos.ZipCode} id = {Object.keys(infos)[5]} type = "number" onChange={handleChange}/>
-                <Input prop = {infos.Country} id = {Object.keys(infos)[6]} type = "text" onChange={handleChange}/>
-                <Input prop = {infos.Email} id = {Object.keys(infos)[7]} type = "email" onChange={handleChange}/>
-                <Input prop = {infos.PhoneNumber} id = {Object.keys(infos)[8]} type = "tel" onChange={handleChange}/>
-                <Input prop = {infos.Description} id = {Object.keys(infos)[9]} type = "textarea" onChange={handleChange}/>
-            </form>
-        )
-    }
-}
-
-export class EducationalExperience extends Component {
-    constructor() {
-        super();
-    }
-    render() {
-        return (
-            <form>
-                <legend>Educational Experience</legend>
-                <Input name = "School Name : " type = "text" />
-                <Input name = "Title of Study : " type = "text" />
-                <Input name = "Degree : " type = "text" />
-                <Input name = "From : " type = "text" />
-                <Input name = "To : " type = "text" />
-            </form>
-        )
-    }
-}
-
-export class ProfessionalExperience extends Component {
-    constructor() {
-        super();
-    }
-    render() {
-        return (
-            <form>
-                <legend>Professional Experience</legend>
-                <Input name = "Position : " type = "text" />
-                <Input name = "Company : " type = "text" />
-                <Input name = "City : " type = "text" />
-                <Input name = "From : " type = "text" />
-                <Input name = "To : " type = "text" />
+                <legend>{section}</legend>
+                {keys.map(key => {
+                    return (
+                    <Input 
+                    key={key}
+                    prop={data[key]}
+                    id={key}
+                    onChange={handleChange}
+                    />
+                    )
+                })}
             </form>
         )
     }
@@ -68,17 +33,17 @@ class Input extends Component{
         super(props);
     }
     render() {
-        const { prop, id, type, onChange } = this.props;
+        const { prop, id, onChange } = this.props;
         const { placeholder, data } = prop; 
         function handleChange(e){
             onChange(e)
         }
         return(
             <label>
-                {type === 'textarea' ? (
+                {id === 'description' ? (
                     <textarea placeholder={placeholder} defaultValue={data} id={id} onChange={handleChange}></textarea>
                 ) : (
-                    <input placeholder={placeholder} defaultValue={data} type={type} id={id} onChange={handleChange}></input>
+                    <input placeholder={placeholder} defaultValue={data} id={id} onChange={handleChange}></input>
                 )}
             </label>
             )
