2895a0a3ebc9f6d2b721bf180012469056113602
bbqbop <dierk.peters@gmail.com>
2023-05-26 10:26:23 -0400

    setup basic data flow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { PersonalInformation, EducationalExperience, ProfessionalExperience } from './Components/Forms'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      PersonalInformation: {
+        FirstName: {placeholder: 'First Name', data: ''},
+        LastName: {placeholder: 'Last Name', data: ''},
+        Title: {placeholder:'Title', data: ''}, 
+        Address: {placeholder:'Address', data: ''},
+        City: {placeholder:'City', data: ''},
+        ZipCode: {placeholder:'Zip Code', data: ''},
+        Country: {placeholder:'Country', data: ''},
+        Email: {placeholder:'Email', data: ''},
+        PhoneNumber: {placeholder:'Phone Number', data: ''},
+        Description: {placeholder:'Description', data: ''},
+      },
+      EducationalExperience: [],
+      ProfessionalExperience: []
+    }
+  } 
+
+  onChange = (e, subFolder) => {
+    const { value, id } = e.target
+   this.setState(prevState => ({
+      [subFolder]: {
+        ...prevState[subFolder],
+        [id]: {
+          ...prevState[subFolder][id],
+          data: value
+        }
+      }
+    })
+   )
+     console.log(this.state);
+  }
+
+  render(){
+
+    return (
+      <div>
+        <PersonalInformation infos={this.state.PersonalInformation} onChange={this.onChange} />
+        {/* <EducationalExperience />
+        <ProfessionalExperience /> */}
+      </div>
+    )
+  }
 }
-
-export default App;
